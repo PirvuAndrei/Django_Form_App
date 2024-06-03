8b1a423796358cabb5c7f2124fc4197094c6946c
Catalin Pirvu <catapirvu98@gmail.com>
2024-06-03 16:48:19 +0300

    Creatinf a Form Model

--- job_application/views.py
+++ job_application/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from .forms import ApplicationForm
 
 def index(request):
+    if request.method == "POST":
+        form = ApplicationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["flast_name"]
+            email = form.cleaned_data["email"]
+            date = form.cleaned_data["date"]
+            occupation = form.cleaned_data["occupation"]
+            print(first_name)
     return render(request, "index.html")
 
